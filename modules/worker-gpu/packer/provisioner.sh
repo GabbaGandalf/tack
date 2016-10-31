de8c0488b39b762a5dcf4f5fa3ebcb05a2691a08
GabbaGandalf <jm.wanka@gmail.com>
2016-10-31 15:53:39 +0100

    change cuda installation which enables mapping of nvidia and cuda libs into container

--- modules/worker-gpu/packer/provisioner.sh
+++ modules/worker-gpu/packer/provisioner.sh
@@ -23,29 +23,32 @@ wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.
 wget http://de.download.nvidia.com/XFree86/Linux-x86_64/367.57/NVIDIA-Linux-x86_64-367.57.run
 chmod +x *run
 mkdir nvidia_installers
-#install nvidia driver (-s silent -a accept license -Z disable noveau driver)
-sudo ./NVIDIA-Linux-x86_64-367.57.run -s -Z -a
-sudo update-initramfs -u
 
 #extract cuda
 ./cuda_8.0.44_linux-run -extract=$PWD/nvidia_installers
+
+#install nvidia driver (-s silent -a accept license -Z disable noveau driver)
+sudo ./NVIDIA-Linux-x86_64-367.57.run -s -Z -a --utility-prefix=/opt/nvidia --opengl-prefix=/opt/nvidia
+sudo update-initramfs -u
+
 #clean-up
 rm -rf *run
 
+#link nvidia libraries
+echo "/opt/nvidia/lib" | sudo tee --append /etc/ld.so.conf
+sudo ldconfig
+
 #install cuda toolkit
 cd nvidia_installers
-sudo ./cuda-linux64-rel-8.0.44-21122537.run -noprompt
+sudo ./cuda-linux64-rel-8.0.44-21122537.run -noprompt -prefix=/opt/cuda
 
-#clean-up
+#link cuda libraries
+echo "/opt/cuda/lib64" | sudo tee --append /etc/ld.so.conf
+sudo ldconfig
+
+#clean up installers
 cd ../
 rm -rf nvidia_installers
-
-#export env variables
-export PATH=/usr/local/cuda-8.0/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
-#make env variables available for normal user
-echo export PATH=/usr/local/cuda-8.0/bin:\$PATH >> $HOME/.bashrc
-echo export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:\$LD_LIBRARY_PATH >> $HOME/.bashrc
 
 #make sure all devices are present
 sudo mv /tmp/nv-devices /opt/bin/
